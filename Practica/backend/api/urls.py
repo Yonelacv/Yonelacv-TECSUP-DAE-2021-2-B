2bbd5ddc025bea984ac3c1aff2efbef0443bceef
Yonelacv <76539570+Yonelacv@users.noreply.github.com>
2021-11-29 00:34:40 -0500

    Delete Practica directory

--- Practica/backend/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('',views.index),
-    path('prestamos',views.prestamos),
-    path('prestamos/<int:prestamos_id>', views.prestamodetalle)
-]
